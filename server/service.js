c335c683ed7aa8c85ed7d92af653344a1fe5bf1b
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-06-05 14:58:17 +0300

    add service registry class

--- server/service.js
+++ server/service.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const ServiceRegistry = require('./serviceRegistry');
 
 const service = express();
+const serviceRegistry = new ServiceRegistry();
 
 service.put('/service/:intent/:port', (req, res, next) => {
   const serviceIntent = req.params.intent;
@@ -10,6 +12,7 @@ service.put('/service/:intent/:port', (req, res, next) => {
     ? `[${req.connection.remoteAddress}]`
     : req.connection.remoteAddress;
 
+  serviceRegistry.add(serviceIntent, serviceIp, servicePort);
   res.json({result: `${serviceIntent} at ${serviceIp}:${servicePort}`});
 });
 
